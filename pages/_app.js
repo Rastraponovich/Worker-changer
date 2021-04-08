a6b896961714b3135f7c367459ac9ebfd10a00f2
Rastrapon <58354560+Rastraponovich@users.noreply.github.com>
2021-04-08 17:55:40 +0300

    context

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
-
+import "../styles/globals.css"
+import { ThingsProvider } from "../components/App/ThingsContext"
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <ThingsProvider value={{}}>
+            <Component {...pageProps} />
+        </ThingsProvider>
+    )
 }
 
 export default MyApp
