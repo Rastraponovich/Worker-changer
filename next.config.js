66dac0d3d9a2543e13393312c18daf23c7dda5a1
Vitaliy Kuroedov <wilde@bk.ru>
2022-01-28 19:45:59 +0300

    release 2.2.0

--- next.config.js
+++ next.config.js
@@ -1,9 +1,21 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const conf = require("./package.json")
+const revision = require("child_process").execSync("git rev-parse HEAD").toString().trim()
+
 const nextConfig = {
     /* config options here */
     reactStrictMode: true,
+    poweredByHeader: false,
+    generateBuildId: async () => {
+        // You can, for example, get the latest git commit hash here
+
+        const version = `prod-${conf.version}.${revision}`
+        console.log(version)
+        return version
+    },
 }
 
 module.exports = nextConfig
